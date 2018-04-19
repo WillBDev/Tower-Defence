6a58db3729b531ee352d72916c7606dc16b93fe7
WillBDev <willbdev@gmail.com>
2018-04-19 10:04:46 -0400

    Enemy AI Added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;               // Reference to the player's position.
+	int playerHealth=10;      // Reference to the player's health.
+	int enemyHealth=10;        // Reference to this enemy's health.             // Reference to the nav mesh agent.
+	public float Speed = 0.8f;
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.Find("Character").transform;
+		/*playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent <EnemyHealth> ();*/
+	}
+
+
+	void Update ()
+	{
+		// If the enemy and the player have health left...
+		Vector2 velocity = new Vector2((transform.position.x - player.position.x) * Speed, (transform.position.y - player.position.y) * Speed);
+		GetComponent<Rigidbody2D>().velocity = -velocity;
+	} 
+}
